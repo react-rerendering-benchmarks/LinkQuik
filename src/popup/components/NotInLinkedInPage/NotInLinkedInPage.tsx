13c622fccae28adadf4a317d27fc037c69298fd0
Satyajit Gokhale <twisterintwist@gmail.com>
2024-10-02 11:04:08 -0400

    Automated transformation to reduce re-rendering

--- src/popup/components/NotInLinkedInPage/NotInLinkedInPage.tsx
+++ src/popup/components/NotInLinkedInPage/NotInLinkedInPage.tsx
@@ -1,10 +1,8 @@
-import React from 'react'
-
-export const NotInLinkedInPage = () => {
-    return (
-        <div className='mt-10 mx-4 flex flex-col items-center'>
+import { memo } from "react";
+import React from 'react';
+export const NotInLinkedInPage = memo(() => {
+  return <div className='mt-10 mx-4 flex flex-col items-center'>
             <img className='h-20' src="emoji_u1f97a.png" height={38} />
             <p data-testid="main-text" className='text-4xl text-center mt-7 font-[Podkova]'>This is not a LinkedIn search page</p>
-        </div>
-    )
-}
+        </div>;
+});
